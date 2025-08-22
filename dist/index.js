f780d226ddb0ab4a5456186345972b8a64563613
Your Name <mohammedghoneim06@gmail.com>
2025-08-23 01:43:01 +0300

    update repo

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,9 @@ window.onload = readData;
 // create task
 let data = localStorage.getItem("tasks") ? JSON.parse(localStorage.tasks) : [];
 function createTask() {
-    let info = {
-        taskInput: taskInput.value
+    const info = {
+        taskInput: taskInput.value,
+        completed: false
     };
     data.push(info);
     localStorage.setItem("tasks", JSON.stringify(data));
@@ -18,11 +19,32 @@ function createTask() {
 function readData() {
     listContainer.textContent = "";
     for (let i = 0; i < data.length; i++) {
+        // li
         let li = document.createElement("li");
         li.textContent = data[i].taskInput;
+        if (data[i].completed) {
+            li.classList.add("checked");
+        }
         listContainer.appendChild(li);
+        // span
         let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
 }
+// delete and checked **
+listContainer.addEventListener("click", function (e) {
+    let target = e.target;
+    if (target.tagName === "LI") {
+        let index = Array.from(listContainer.children).indexOf(target);
+        data[index].completed = !data[index].completed;
+        localStorage.setItem("tasks", JSON.stringify(data));
+        readData();
+    }
+    else if (target.tagName === "SPAN") {
+        let index = Array.from(listContainer.children).indexOf(target.parentElement);
+        data.splice(index, 1);
+        localStorage.setItem("tasks", JSON.stringify(data));
+        readData();
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
-const taskInput =document.getElementById("task-input") as HTMLInputElement
+const taskInput = document.getElementById("task-input") as HTMLInputElement
 const listContainer = document.getElementById("list-container") as HTMLUListElement
 
 window.onload=readData
-
+interface Task{
+    taskInput:string
+    completed:boolean
+}
 // create task
-let data = localStorage.getItem("tasks")?JSON.parse(localStorage.tasks):[]
-function createTask() {
-    let info={
-        taskInput:taskInput.value
+let data:Task[] = localStorage.getItem("tasks")?JSON.parse(localStorage.tasks) as Task[]:[]
+function createTask():void {
+    const info:Task={
+       taskInput:taskInput.value,
+       completed:false
     }
     data.push(info)
     localStorage.setItem("tasks",JSON.stringify(data))
@@ -17,14 +21,36 @@ function createTask() {
 }
 
 //readDate
-function readData() {
+function readData():void {
     listContainer.textContent=""
     for (let i = 0; i < data.length; i++) { 
-        let li = document.createElement("li")
+        // li
+        let li = document.createElement("li") as HTMLLIElement
         li.textContent=data[i].taskInput
+        if (data[i].completed) {
+            li.classList.add("checked")
+        } 
         listContainer.appendChild(li) 
-        let span = document.createElement("span")
+        // span
+        let span = document.createElement("span") as HTMLSpanElement
         span.innerHTML="\u00d7"
         li.appendChild(span)
        } 
 }
+
+// delete and checked **
+listContainer.addEventListener("click",function (e:MouseEvent):void {
+    let target = e.target as HTMLElement
+    if (target.tagName==="LI") {
+        let index = Array.from(listContainer.children).indexOf((target as HTMLLIElement))
+        data[index].completed=!data[index].completed
+        localStorage.setItem("tasks",JSON.stringify(data))
+        readData()
+    }else if (target.tagName==="SPAN") {
+        let index = Array.from(listContainer.children).indexOf((target as HTMLSpanElement).parentElement as HTMLLIElement)
+        data.splice(index,1)
+        localStorage.setItem("tasks",JSON.stringify(data))
+        readData()
+    }
+    
+})
